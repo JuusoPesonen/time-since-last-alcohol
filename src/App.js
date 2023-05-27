f8e7ff2f7d16ea9010805b80728c08315907ddb7
JuusoPesonen <c2peju00@students.oamk.fi>
2023-05-27 14:37:42 +0300

    Modified code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import TimeSince from './TimeSince';
 
 function App() {
   return (
     <div className="App">
+      <TimeSince />
     </div>
   );
 }
